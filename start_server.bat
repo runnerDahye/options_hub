dd23913d4c398afce10e2414d7825d92780ef75f
이다혜 <dh199606@naver.com>
2025-09-16 18:44:04 +0900

    [2025/09/16]수정

---
+++ start_server.bat
@@ -0,0 +1,12 @@
+@echo off
+echo 유진투자선물 데모 서버를 시작합니다...
+echo.
+echo 접속 주소:
+echo http://58.229.223.138:3000
+echo http://58.229.223.164:3000
+echo http://localhost:3000
+echo.
+echo 서버를 중지하려면 Ctrl+C를 누르세요.
+echo.
+node server.js
+pause
